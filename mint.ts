6d7979439ce05db51d0a3eb06c36c0dbb207de84
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-03-07 17:29:41 +0530

    added mint data and metadata

--- mint.ts
+++ mint.ts
@@ -50,5 +50,87 @@ const UploadMetadata = async(wallet: Keypair, tokenMetadata: UploadMetadataInput
     const {uri} = await metaplex.nfts().uploadMetadata(tokenMetadata);
     console.log(`Arweave URL: `, uri);
     return uri;
-    
 }
+
+const createNewMintTransaction = async (connection:Connection, payer:Keypair, mintKeypair: Keypair, destinationWallet: PublicKey, mintAuthority: PublicKey, freezeAuthority: PublicKey)=>{
+    //Get the minimum lamport balance to create a new account and avoid rent payments
+    const requiredBalance = await getMinimumBalanceForRentExemptMint(connection);
+    //metadata account associated with mint
+    const metadataPDA = await findMetadataPda(mintKeypair.publicKey);
+    //get associated token account of your wallet
+    const tokenATA = await getAssociatedTokenAddress(mintKeypair.publicKey, destinationWallet);   
+    
+
+    const createNewTokenTransaction = new Transaction().add(
+        SystemProgram.createAccount({
+            fromPubkey: payer.publicKey,
+            newAccountPubkey: mintKeypair.publicKey,
+            space: MINT_SIZE,
+            lamports: requiredBalance,
+            programId: TOKEN_PROGRAM_ID,
+        }),
+        createInitializeMintInstruction(
+            mintKeypair.publicKey, //Mint Address
+            MINT_CONFIG.numDecimals, //Number of Decimals of New mint
+            mintAuthority, //Mint Authority
+            freezeAuthority, //Freeze Authority
+            TOKEN_PROGRAM_ID
+        ),
+        createAssociatedTokenAccountInstruction(
+            payer.publicKey, //Payer 
+            tokenATA, //Associated token account 
+            payer.publicKey, //token owner
+            mintKeypair.publicKey, //Mint
+        ),
+        createMintToInstruction(
+            mintKeypair.publicKey, //Mint
+            tokenATA, //Destination Token Account
+            mintAuthority, //Authority
+            MINT_CONFIG.numberTOkens * Math.pow(10, MINT_CONFIG.numDecimals),//number of tokens
+        ),
+        createCreateMetadataAccountV2Instruction({
+                metadata: metadataPDA, 
+                mint: mintKeypair.publicKey, 
+                mintAuthority: mintAuthority,
+                payer: payer.publicKey,
+                updateAuthority: mintAuthority,
+            },
+            { createMetadataAccountArgsV2: 
+                { 
+                data: ON_CHAIN_METADATA, 
+                isMutable: true 
+                } 
+            }
+        )
+    );
+
+    return createNewTokenTransaction;
+}
+
+const main = async() => {
+    console.log(`---STEP 1: Upload Metadata---`);
+    const userWallet = Keypair.fromSecretKey(new Uint8Array(secret));
+    let metadataUri = await UploadMetadata(userWallet, MY_TOKEN_METADATA);
+    ON_CHAIN_METADATA.uri = metadataUri;
+
+    console.log(`---STEP 2: Creating Mint Transaction---`);
+    let mintKeypair = Keypair.generate();
+    console.log(`New Mint Address:`, mintKeypair.publicKey.toString());
+
+    const newMintTransaction:Transaction = await createNewMintTransaction(
+        solanaConnection,
+        userWallet,
+        mintKeypair,
+        userWallet.publicKey,
+        userWallet.publicKey,
+        userWallet.publicKey,
+    );
+
+    console.log(`---STEP 3: Executing Mint Transaction---`);
+    const transactionId = await solanaConnection.sendTransaction(newMintTransaction, [userWallet, mintKeypair]);
+    console.log(`Transaction ID: `, transactionId);
+    console.log(`Succesfully minted ${MINT_CONFIG.numberTOkens} ${ON_CHAIN_METADATA.symbol} to $${userWallet.publicKey.toString()}.`);
+    console.log(`View Transaction: https://explorer.solana.com/tx/${transactionId}?cluster=devnet`);
+    console.log(`View Token Mint: https://explorer.solana.com/address/${mintKeypair.publicKey.toString()}?cluster=devnet`);
+}
+main();
